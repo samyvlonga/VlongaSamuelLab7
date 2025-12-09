744c417660429a5e0ffe725c652db20fae6fb91d
samyvlonga <samyvlonga@gmail.com>
2025-12-09 11:49:00 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-
+using VlongaSamuelLab7.Data;
 namespace VlongaSamuelLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
---
+++ Models/ShopList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+
+namespace VlongaSamuelLab7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
