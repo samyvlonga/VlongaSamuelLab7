618153ba1c1c1aecf57a5efa56337109e36bc8af
samyvlonga <samyvlonga@gmail.com>
2026-01-09 18:32:18 +0200

    lab9

---
+++ Models/ListProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace VlongaSamuelLab7.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+        public int ProductID { get; set; }
+    }
+}
